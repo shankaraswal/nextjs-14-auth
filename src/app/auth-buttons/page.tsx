f84d9dca40a24615b7e7d02aaba7719cd854c2c7
ShankarAswal <shankaraswal@gmail.com>
2025-10-27 14:39:11 +0000

    Export sign-in button components for Google, GitHub, and Email

--- src/app/auth-buttons/page.tsx
+++ src/app/auth-buttons/page.tsx
@@ -2,25 +2,24 @@
 
 import { signIn } from "next-auth/react";
 
-const GoogleSignInButton = () => {
+export const GoogleSignInButton = () => {
   const handleClick = () => {
     signIn("google");
   };
   return (
     <button
       onClick={handleClick}
-      className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl  transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
+      className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
     >
       <span className="ml-4">Continue with Google</span>
     </button>
   );
 };
 
-const GithubSignInButton = () => {
+export const GithubSignInButton = () => {
   const handleClick = () => {
     signIn("github");
   };
-
   return (
     <button
       onClick={handleClick}
@@ -31,20 +30,16 @@ const GithubSignInButton = () => {
   );
 };
 
-const CredentialsSignInButton = () => {
+export const CredentialsSignInButton = () => {
   const handleClick = () => {
     signIn();
   };
-
   return (
     <button
       onClick={handleClick}
       className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
     >
-      {/* <Image src={githubLogo} alt="Github Logo" width={20} height={20} /> */}
       <span className="ml-4">Continue with Email</span>
     </button>
   );
 };
-
-export { GoogleSignInButton, GithubSignInButton, CredentialsSignInButton };
